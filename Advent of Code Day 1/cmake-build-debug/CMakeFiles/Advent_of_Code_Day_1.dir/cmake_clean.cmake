737009f1928090d9d24e20890fd6dfb48c37224c
rasp <raspbian@gmx.at>
2017-12-01 20:33:04 +0100

    AoC, day 1, challanges A,B, worktime around 30 minutes (with debugging excluded)

---
+++ Advent of Code Day 1/cmake-build-debug/CMakeFiles/Advent_of_Code_Day_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Advent_of_Code_Day_1.dir/main.cpp.o"
+  "Advent_of_Code_Day_1.pdb"
+  "Advent_of_Code_Day_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advent_of_Code_Day_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
